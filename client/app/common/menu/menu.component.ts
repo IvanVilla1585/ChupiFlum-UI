280f81d4a6bc599f01bf6bdbe03b417081268c3d
Ivan <trezeguet55@gmail.com>
2017-01-14 10:24:16 -0500

    <menu.component.ts> add function getDataMenu and the parameters dataMenu and isHidden

--- client/app/common/menu/menu.component.ts
+++ client/app/common/menu/menu.component.ts
@@ -7,9 +7,112 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  dataMenu: any [];
+  isHidden: boolean;
+
+  constructor() {
+    this.dataMenu = [];
+    this.isHidden = true;
+  }
 
   ngOnInit() {
+    this.getDataMenu();
+  }
+
+  getDataMenu(){
+    this.dataMenu = [
+      {
+        label: 'Home',
+        url: '',
+        subMenu: []
+      },
+      {
+        label: 'Permisos',
+        url: '',
+        subMenu: [
+          {
+            label: 'Usuarios',
+            url: '',
+          },
+          {
+            label: 'Roles',
+            url: '',
+          },
+          {
+            label: 'Permisos',
+            url: '',
+          }
+        ]
+      },
+      {
+        label: 'Compras',
+        url: '',
+        subMenu: [
+          {
+            label: 'Proveedores',
+            url: '',
+          },
+          {
+            label: 'Pedidos',
+            url: '',
+          },
+          {
+            label: 'Kardex Pedidos',
+            url: '',
+          }
+        ]
+      },
+      {
+        label: 'Producción',
+        url: '',
+        subMenu: [
+          {
+            label: 'Ordenes de Producción',
+            url: '',
+          },
+          {
+            label: 'Maquinas',
+            url: '',
+          },
+          {
+            label: 'Unidades de Medida',
+            url: '',
+          }
+        ]
+      },
+      {
+        label: 'Inventario',
+        url: '',
+        subMenu: [
+          {
+            label: 'Materia Prima',
+            url: '',
+          },
+          {
+            label: 'Producto Terminado',
+            url: '',
+          }
+        ]
+      },
+      {
+        label: 'Ayudas',
+        url: '',
+        subMenu: [
+          {
+            label: 'Acerca de',
+            url: '',
+          },
+          {
+            label: 'Manual de Usuario',
+            url: '',
+          }
+        ]
+      }
+    ]
+  }
+
+  openSubMenu(){
+    this.isHidden = !this.isHidden;
   }
 
 }
