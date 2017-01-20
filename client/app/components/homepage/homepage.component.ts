0446448cad7a6fc9aea52a4cc1a16f36c780339c
Ivan <trezeguet55@gmail.com>
2017-01-20 18:46:03 -0500

    <homepage.component.ts> moved from account folder

--- client/app/components/homepage/homepage.component.ts
+++ client/app/components/homepage/homepage.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
-  templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.styl']
+  templateUrl: 'homepage.component.html',
+  styleUrls: ['homepage.component.styl']
 })
 export class HomepageComponent implements OnInit {
 
