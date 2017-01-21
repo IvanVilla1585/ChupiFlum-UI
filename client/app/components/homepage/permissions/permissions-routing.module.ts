31cddff4059dd5fdda97fbc4be24532a6f07ba72
Ivan <trezeguet55@gmail.com>
2017-01-21 15:44:19 -0500

    <permissions-routing.module.ts> add route roles

--- client/app/components/homepage/permissions/permissions-routing.module.ts
+++ client/app/components/homepage/permissions/permissions-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PermissionsComponent } from './permissions.component';
+import { RolesComponent } from './roles/roles.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'roles',
+    component: RolesComponent
+  }
+];
+
+export const routesPermissionsComponents = [
+  RolesComponent
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
