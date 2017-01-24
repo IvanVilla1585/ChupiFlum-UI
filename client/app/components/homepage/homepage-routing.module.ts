26c873e7141692e42e014dfee56248a879b1f684
ivan <trezeguet55@gmail.com>
2017-01-23 22:53:45 -0500

    <homepage-routing.module.ts> add routes permissions

--- client/app/components/homepage/homepage-routing.module.ts
+++ client/app/components/homepage/homepage-routing.module.ts
@@ -1,24 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomepageComponent } from './homepage.component'
-import { PermissionsComponent } from './permissions/permissions.component'
+import { HomepageComponent } from './homepage.component';
+import { PermissionsComponent } from './permissions/permissions.component';
+import { routesPermissions, routesPermissionsComponents } from './permissions/permissions-routing.module';
 
 const routes: Routes = [
   {
     path: 'menu',
     component: HomepageComponent,
-    children: [
-      {
-        path: 'permissions',
-        component: PermissionsComponent
-      }
-    ]
+    children: routesPermissions
   }
 ];
 
 export const routesComponents = [
   HomepageComponent,
-  PermissionsComponent
+  routesPermissionsComponents
 ];
 
 @NgModule({
