613b10f616c5ea15e5d901c0299fdc53f6438fa4
ivan <trezeguet55@gmail.com>
2017-02-08 21:39:11 -0500

    <homepage.module.ts> add permission module

--- client/app/components/homepage/homepage.module.ts
+++ client/app/components/homepage/homepage.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageRoutingModule, routesComponents } from './homepage-routing.module';
-import { PermissionsComponent } from './permissions/permissions.component';
 import {MenuComponent} from "../../common/menu/menu.component";
 import {HeaderComponent} from "../../common/header/header.component";
+import {PermissionsModule} from "./permissions/permissions.module";
 
 @NgModule({
   imports: [
     CommonModule,
-    HomepageRoutingModule
+    HomepageRoutingModule,
+    PermissionsModule
   ],
   declarations: [routesComponents, MenuComponent, HeaderComponent],
   exports: [routesComponents, MenuComponent, HeaderComponent]
