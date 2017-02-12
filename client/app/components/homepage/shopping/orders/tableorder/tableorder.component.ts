cfac9feeb54732edd4802fd2aabe896f6a519b78
ivan <trezeguet55@gmail.com>
2017-02-11 22:44:31 -0500

    created tableorders component

---
+++ client/app/components/homepage/shopping/orders/tableorder/tableorder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tableorder',
+  templateUrl: './tableorder.component.html',
+  styleUrls: ['./tableorder.component.styl']
+})
+export class TableorderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
