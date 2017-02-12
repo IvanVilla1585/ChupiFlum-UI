e4003f86afe3a6431f3f0d5cb6d48d9293b51fff
ivan <trezeguet55@gmail.com>
2017-02-11 22:43:23 -0500

    created kardexorder component

---
+++ client/app/components/homepage/shopping/kardexorders/kardexorder/kardexorder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kardexorder',
+  templateUrl: './kardexorder.component.html',
+  styleUrls: ['./kardexorder.component.styl']
+})
+export class KardexorderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
