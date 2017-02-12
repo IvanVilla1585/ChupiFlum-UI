7329715df8fbd821c1a189797bf7ce5fb348e1dd
ivan <trezeguet55@gmail.com>
2017-02-12 06:06:04 -0500

    create machine components

---
+++ client/app/components/homepage/production/machine/machine.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-machine',
+  templateUrl: './machine.component.html',
+  styleUrls: ['./machine.component.styl']
+})
+export class MachineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
