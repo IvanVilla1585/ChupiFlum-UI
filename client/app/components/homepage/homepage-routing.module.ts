5b06cac2d6d0c9c26bd2f503328071165c4a7e4c
ivan <trezeguet55@gmail.com>
2017-02-11 22:21:01 -0500

    <homepage-routing.module.ts> add routesShopping

--- client/app/components/homepage/homepage-routing.module.ts
+++ client/app/components/homepage/homepage-routing.module.ts
@@ -3,18 +3,23 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage.component';
 import { PermissionsComponent } from './permissions/permissions.component';
 import { routesPermissions, routesPermissionsComponents } from './permissions/permissions-routing.module';
+import {routesShopping, routesShoppingComponents} from "./shopping/shopping-routing.module";
 
 const routes: Routes = [
   {
     path: 'menu',
     component: HomepageComponent,
-    children: routesPermissions
+    children: [
+      routesPermissions,
+      routesShopping
+    ]
   }
 ];
 
 export const routesComponents = [
   HomepageComponent,
-  routesPermissionsComponents
+  routesPermissionsComponents,
+  routesShoppingComponents
 ];
 
 @NgModule({
