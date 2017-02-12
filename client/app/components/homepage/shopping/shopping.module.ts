270c86766eb18224a8297feadf01310e302b650c
ivan <trezeguet55@gmail.com>
2017-02-11 22:45:20 -0500

    <shopping.module.ts> declarations components

--- client/app/components/homepage/shopping/shopping.module.ts
+++ client/app/components/homepage/shopping/shopping.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ShoppingComponent } from './shopping.component';
 import { SupplierComponent } from './supplier/supplier/supplier.component';
 import { TablesupplierComponent } from './supplier/tablesupplier/tablesupplier.component';
+import { OrderComponent } from './orders/order/order.component';
+import { TableorderComponent } from './orders/tableorder/tableorder.component';
+import { KardexorderComponent } from './kardexorders/kardexorder/kardexorder.component';
+import { TablekardexorderComponent } from './kardexorders/tablekardexorder/tablekardexorder.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SupplierComponent, TablesupplierComponent]
+  declarations: [SupplierComponent, TablesupplierComponent, OrderComponent, TableorderComponent, KardexorderComponent, TablekardexorderComponent]
 })
 export class ShoppingModule { }
