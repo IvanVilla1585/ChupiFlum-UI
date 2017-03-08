3346ed625af3ddac42406d40ee4f52ba3fdc605c
ivan <trezeguet55@gmail.com>
2017-03-08 15:05:50 -0500

    <login.component.ts> add function sigIn

--- client/app/components/login/login.component.ts
+++ client/app/components/login/login.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {FormBuilder, Validators, FormGroup, NgForm} from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import {User} from "./user";
 
 @Component({
   selector: 'app-login',
   templateUrl: 'login.component.html',
-  styleUrls: ['login.component.styl']
+  styleUrls: ['login.component.styl'],
+  providers: [AuthService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public user : User;
+
+  public username: string;
+  public password: string;
+  public loginForm: FormGroup;
+
+  constructor(private authService: AuthService) {
+    this.username = "Leidy";
+  }
 
   ngOnInit() {
+  }
+
+  sigIn(event){
+    event.preventDefault();
+    debugger
+    console.log();
+    let user = {
+      username: this.username,
+      password: this.password
+    };
+    this.authService
+      .loginUser(user)
+      .then(res => {
+        debugger
+        this.user = res;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
 }
