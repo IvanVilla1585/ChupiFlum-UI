208331779c5abda7dddc2f177a49e70fe5fbbdcd
Ivan Villa <trezeguet55@gmail.com>
2017-03-26 18:51:40 -0500

    <auth.service.ts> refactor code

--- client/app/services/auth/auth.service.ts
+++ client/app/services/auth/auth.service.ts
@@ -1,26 +1,66 @@
 import { Injectable } from '@angular/core';
-import {Http, Headers, RequestOptions} from "@angular/http";
-import 'rxjs/add/operator/toPromise';
+import {Http, Headers, RequestOptions, Response} from "@angular/http";
+import {CookieService} from 'angular2-cookie/core';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
 import {User} from "../../components/login/user";
+import {Observable} from "rxjs";
 
 @Injectable()
 export class AuthService {
 
-  private url : string;
-  private headers = new Headers({'Content-Type': 'application/json'});
-  private options = new RequestOptions({ headers: this.headers });
+  private headers : Headers;
+  private options : RequestOptions;
+  private client_id: string;
+  private client_secret: string;
+  private grant_type: string;
+  private BASE_URL: string;
 
-  constructor(private http: Http) {
+  constructor(private _http: Http, private _cookieService: CookieService) {
 
-    this.url = 'http://localhost:8000/api/sigin';
+    this.BASE_URL = 'http://localhost:8000/api';
+    this.headers = new Headers({
+      'Accept': 'application/json',
+      'Content-Type': 'application/json; charset=utf-8'
+    });
+    this.options = new RequestOptions({ headers: this.headers });
+    this.grant_type = 'password';
+    this.client_id = 'Xb3mTjNVtkNs8N6OxRdpJRwfVNgXlGd2NTwIWXiA';
+    this.client_secret = 'MPWU4Vge9xw1ocmDGcLLNzYiVW5lp7n9Jc95le4zsIjtAB2siakzAcq5wZzy4eOLNDzcmA8uSRy11jyQBYjorV0muCzRCGab5jHvm7GcoODxpBp0Quc8w4ZmFERbTwS4';
   }
 
+  login(data): Observable<any> {
+    return this._http.post(`${this.BASE_URL}/sigin`, JSON.stringify(data), this.options)
+      .map((res: any) => res.json())
+      .mergeMap((response: any) => {
+      debugger
+        let json = this.completeDataToken(data);
+        return this._http.post(`${this.BASE_URL}/token/`, JSON.stringify(json), this.options)
+        .map((res: any) => {
+          let token = res.json();
+          response.user.access_token = token.access_token;
+          response.user.token_type = token.token_type;
+          response.user.expires_in = token.expires_in;
+          response.user.refresh_token = token.refresh_token;
+          response.user.last_login = new Date()
+          return response;
+        });
+      });
+  }
 
-  loginUser(data): Promise<User> {
-    return this.http.post(this.url, JSON.stringify(data), this.options)
-      .toPromise()
-      .then(response => response.json().data as User)
-      .catch(this.handleError);
+  completeDataToken(data){
+    data.grant_type = this.grant_type;
+    data.client_id = this.client_id;
+    data.client_secret = this.client_secret;
+    return data;
+  }
+
+  storeCookie(user){
+    this._cookieService.put('user', JSON.stringify(user));
+  }
+
+  getCookie(){
+    return this._cookieService.get("user");
   }
 
   private handleError(error: any): Promise<any> {
