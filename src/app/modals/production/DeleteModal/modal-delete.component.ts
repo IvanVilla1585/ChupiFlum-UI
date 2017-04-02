950a35ae4cd4bb4c6f570221fe3c8a75de55bdab
Ivan Villa <trezeguet55@gmail.com>
2017-04-02 15:46:36 -0500

    <model-delete.component.ts> a new file is created

---
+++ src/app/modals/production/DeleteModal/modal-delete.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-delete',
+  templateUrl: 'modal-delete.component.html',
+  styleUrls: ['modal-delete.component.styl']
+})
+export class ModalDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
