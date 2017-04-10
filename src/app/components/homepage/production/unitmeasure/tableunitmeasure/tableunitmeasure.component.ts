c484f5755d5a89162b8a5db1b1ff281d1382b027
Ivan Villa <trezeguet55@gmail.com>
2017-04-09 20:27:00 -0500

    <tableunitmeasure.component.ts> add getUnits function

--- src/app/components/homepage/production/unitmeasure/tableunitmeasure/tableunitmeasure.component.ts
+++ src/app/components/homepage/production/unitmeasure/tableunitmeasure/tableunitmeasure.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {UnitmeasureService} from "../../../../../services/production/unitmeasure/unitmeasure.service";
 
 @Component({
   selector: 'app-tableunitmeasure',
   templateUrl: './tableunitmeasure.component.html',
-  styleUrls: ['./tableunitmeasure.component.styl']
+  styleUrls: ['./tableunitmeasure.component.styl'],
+  providers: [UnitmeasureService]
 })
 export class TableunitmeasureComponent implements OnInit {
 
-  constructor() { }
+  public units: any [];
+
+  constructor(private _unitmeasureService: UnitmeasureService) {
+    this.units = [];
+  }
 
   ngOnInit() {
+    this.getUnits();
+  }
+
+  getUnits(){
+    this._unitmeasureService.list()
+      .subscribe(
+        (res) => {
+          this.units = res.json();
+        },
+        (err) => {
+          this.units = [];
+        }
+      );
   }
 
 }
