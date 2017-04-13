a9df25ceae200d65b1f015beae3c06658440f908
Ivan Villa <trezeguet55@gmail.com>
2017-04-13 01:07:35 -0500

    a new edit unit measure component is created

---
+++ src/app/modals/production/edit-unit-measure/edit-unit-measure.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-unit-measure',
+  templateUrl: './edit-unit-measure.component.html',
+  styleUrls: ['./edit-unit-measure.component.styl']
+})
+export class EditUnitMeasureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
