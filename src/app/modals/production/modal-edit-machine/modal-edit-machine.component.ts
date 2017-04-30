2d4f2c00ca3a03c4fe929fc3f328f40debce32f9
Ivan Villa <trezeguet55@gmail.com>
2017-04-30 16:51:18 -0500

    the modal-edit-machine component is created

---
+++ src/app/modals/production/modal-edit-machine/modal-edit-machine.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-edit-machine',
+  templateUrl: './modal-edit-machine.component.html',
+  styleUrls: ['./modal-edit-machine.component.styl']
+})
+export class ModalEditMachineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
