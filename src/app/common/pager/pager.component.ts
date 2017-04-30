39ed9a66d92dfccf0a9be9fd79e12d57038c48c5
Ivan Villa <trezeguet55@gmail.com>
2017-04-29 21:04:51 -0500

    <pager.component.ts> the functionality of component is created

--- src/app/common/pager/pager.component.ts
+++ src/app/common/pager/pager.component.ts
@@ -1,15 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {UnitmeasureService} from "../../services/production/unitmeasure/unitmeasure.service";
 
 @Component({
   selector: 'app-pager',
   templateUrl: './pager.component.html',
-  styleUrls: ['./pager.component.styl']
+  styleUrls: ['./pager.component.styl'],
+  providers: [UnitmeasureService]
 })
 export class PagerComponent implements OnInit {
 
-  constructor() { }
+  public baseUrl: string;
+
+  @Input()
+  public pages: any[];
+  @Input()
+  public totalPages: number;
+  @Input()
+  public nextPage: string;
+  @Input()
+  public previusPage: string;
+
+  @Output()
+  updateDataPager = new EventEmitter();
+
+  constructor(private _unitmeasureService: UnitmeasureService) {
+    this.baseUrl = '';
+    this.pages = [];
+  }
 
   ngOnInit() {
+    this.baseUrl = 'http://localhost:8000/api/unidadesmedidas/?page=';
+  }
+
+
+
+  updateData(data){
+    let newData = {
+      totalPages: data.count,
+      nextPage: data.next,
+      previusPage: data.previous,
+      units: data.results
+    };
+    this.updateDataPager.emit(newData);
+  }
+
+  updateStep(type){
+    debugger
+    let url: string = '';
+    switch (type){
+      case 'next':
+        url = this.nextPage;
+        break;
+      case 'back':
+        url = this.previusPage;
+        break;
+      case 'first':
+        url = `${this.baseUrl}1`;
+        break;
+      case 'last':
+        url = `${this.baseUrl}${this.pages.length}`;
+        break;
+      default:
+        url = `${this.baseUrl}1`;
+        break;
+    }
+    if (!url) return;
+    this._unitmeasureService.listPage(url)
+      .subscribe(
+        (res) => {
+          this.updateData(res.json());
+        },
+        (err) => {
+          let newData = {
+            totalPages: 0,
+            nextPage: '',
+            previusPage: '',
+            units: []
+          };
+          this.updateDataPager.emit(newData);
+        }
+      )
   }
 
 }
