8a32a95e4336cb3454ebfa5eeac0ecac3e804779
Ivan Villa <trezeguet55@gmail.com>
2017-04-29 21:19:48 -0500

    <unitmeasure.service.ts> New methods are created

--- src/app/services/production/unitmeasure/unitmeasure.service.ts
+++ src/app/services/production/unitmeasure/unitmeasure.service.ts
@@ -21,7 +21,23 @@ export class UnitmeasureService {
     return this._httpService.get(`${this.BASE_URL}/unidadesmedidas/`);
   }
 
+  listPage(url: string): Observable<Response>{
+    return this._httpService.get(url);
+  }
+
   save(data): Observable<Response>{
     return this._httpService.post(`${this.BASE_URL}/unidadesmedidas/`, JSON.stringify(data));
+  }
+
+  searchUnitMeasure(search): Observable<Response>{
+    return this._httpService.get(`${this.BASE_URL}/unidadesmedidas/?search=${search}`);
+  }
+
+  update(data: any): Observable<Response>{
+    return this._httpService.put(`${this.BASE_URL}/unidadesmedidas/${data.id}/`, JSON.stringify(data));
+  }
+
+  changeStatus(id: number): Observable<Response>{
+    return this._httpService.delete(`${this.BASE_URL}/unidadesmedidas/${id}/`, JSON.stringify({}));
   }
 }
