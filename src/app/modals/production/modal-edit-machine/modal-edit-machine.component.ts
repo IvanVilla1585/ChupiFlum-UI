4a37df1ea9a36100e4920eef08d676cb6d56143f
Ivan Villa <trezeguet55@gmail.com>
2017-04-30 20:08:36 -0500

    <modal-edit-machine.component.ts> the functionality of component is created

--- src/app/modals/production/modal-edit-machine/modal-edit-machine.component.ts
+++ src/app/modals/production/modal-edit-machine/modal-edit-machine.component.ts
@@ -1,15 +1,96 @@
 import { Component, OnInit } from '@angular/core';
+import {Validators, FormBuilder, FormGroup} from "@angular/forms";
+import {MachineService} from "../../../services/production/machine.service";
+import {MdDialogRef} from "@angular/material";
 
 @Component({
   selector: 'app-modal-edit-machine',
   templateUrl: './modal-edit-machine.component.html',
-  styleUrls: ['./modal-edit-machine.component.styl']
+  styleUrls: ['./modal-edit-machine.component.styl'],
+  providers: [MachineService]
 })
 export class ModalEditMachineComponent implements OnInit {
 
-  constructor() { }
+  public machineForm: FormGroup;
+  public units: any [];
+  public title: string;
+  public errorMessage: string;
+  public data: any;
+  public dialogRef: MdDialogRef<ModalEditMachineComponent>;
 
-  ngOnInit() {
+  constructor(
+    private _machineService: MachineService,
+    private fb: FormBuilder
+  ) {
+    this.units = [];
   }
 
+  ngOnInit() {
+    this.errorMessage = '';
+    this.createForm();
+    this.getUnits();
+    this.setDataForm();
+  }
+
+  createForm() {
+    this.machineForm = this.fb.group({ // <-- the parent FormGroup
+      id: ['', Validators.required ],
+      name: ['', Validators.required ],
+      description: '',
+      quantity: ['', Validators.required ],
+      unit: ['', Validators.required ]
+    });
+  }
+
+  getUnits(){
+    this._machineService.getUnits().subscribe(
+      (res) => {
+        let data = res.json();
+        this.units = data.results;
+      },
+      (err) => {
+        this.units = [];
+        console.log(err.json())
+      }
+    );
+  }
+
+  setDataForm(){
+    this.machineForm.setValue({
+      id: this.data.id,
+      name: this.data.nombre,
+      description: this.data.descripcion,
+      quantity: this.data.capacidad,
+      unit: this.data.unidad_medida.id
+    });
+  }
+
+  update(){
+    let data = {
+      machine: {},
+      valid: false
+    };
+    if (this.machineForm['_status'] === 'VALID') {
+      let machine = {
+        id: this.machineForm.get('id').value,
+        nombre: this.machineForm.get('name').value,
+        descripcion: this.machineForm.get('description').value,
+        capacidad: this.machineForm.get('quantity').value,
+        unidad_medida: this.machineForm.get('unit').value
+      };
+      data.machine = machine;
+      data.valid = true;
+      this.dialogRef.close(data);
+    }else{
+      this.errorMessage = 'Los campos marcados con * son obligatorios'
+    }
+  }
+
+  closeModal(){
+    let data = {
+      machine: {},
+      valid: false
+    };
+    this.dialogRef.close(data);
+  }
 }
