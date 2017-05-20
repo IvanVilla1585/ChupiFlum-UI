ff2742de0d951692436c497915b6428c2b767fb8
Ivan Villa <trezeguet55@gmail.com>
2017-05-20 00:32:58 -0500

    <login.component.ts> the functionalities of toast are implemented

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, ViewContainerRef} from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
 import { ShowFeedback } from '../../libs/showFeedback';
 import {User} from "./user";
+import {ToastsManager} from "ng2-toastr";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   moduleId: module.id,
@@ -14,30 +16,59 @@ import {User} from "./user";
 export class LoginComponent implements OnInit {
 
   public user : any;
+  public loginForm: FormGroup;
 
-  constructor(private authService: AuthService, private showFeedback: ShowFeedback, private _router: Router) {
+  constructor(
+    private authService: AuthService,
+    private _container: ViewContainerRef,
+    private _router: Router,
+    private _toast: ToastsManager,
+    private fb: FormBuilder
+  )
+  {
+    this._toast.setRootViewContainerRef(_container);
   }
 
   ngOnInit() {
-    this.user = {}
+    this.user = {};
+    this.createForm();
+  }
+
+  createForm() {
+    this.loginForm = this.fb.group({ // <-- the parent FormGroup
+      username: ['', Validators.required ],
+      password: ['', Validators.required ]
+    });
   }
 
   sigIn(event){
-    event.preventDefault();
-    this.authService.login(this.user)
-      .subscribe(
-        json => {
-          if (json.status === 200) {
-            this.user = json.user;
-            this.authService.storeCookie(this.user);
-            this._router.navigate(['/menu']);
-          }else{
-            this.showFeedback.createMessage(json.message);
+    debugger
+    if (this.loginForm.status === 'VALID') {
+      let data = {
+        username: this.loginForm.get('username').value,
+        password: this.loginForm.get('password').value
+      };
+      this.authService.login(data)
+        .subscribe(
+          json => {
+            if (json.status === 200) {
+              this.user = json.user;
+              this.authService.storeCookie(this.user);
+              this._router.navigate(['/menu']);
+            }
+          },
+          err => {
+            this.user = {};
+            let data = err.json();
+            if (data.status === 500 && data.message)
+              this._toast.error(data.message, 'Login!');
+            else
+              this._toast.error('Disculpe, ocurrio un error', 'Login!');
           }
-        },
-        err => {
-          this.user = {};
-        });
+        );
+    }else{
+      this._toast.info('Todos los campos marcados con * son obligatorios', 'Login!');
+    }
   }
 
 }
