6643b7a9a00afe7565616ed6f92ea122a45c5933
Ivan Villa <trezeguet55@gmail.com>
2017-09-12 21:38:51 -0500

    <shopping modal-edit-provider>
    create new component

---
+++ src/app/modals/shopping/modal-edit-provider/modal-edit-provider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-edit-provider',
+  templateUrl: './modal-edit-provider.component.html',
+  styleUrls: ['./modal-edit-provider.component.styl']
+})
+export class ModalEditProviderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
