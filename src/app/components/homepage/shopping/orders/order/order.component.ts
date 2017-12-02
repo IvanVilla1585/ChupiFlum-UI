92e84e6fe31f8809fa42ecb02eb43652ca0de4d1
Ivan Villa <trezeguet55@gmail.com>
2017-12-02 07:57:11 -0500

    <shopping order.component.ts>
    create functionalities

--- src/app/components/homepage/shopping/orders/order/order.component.ts
+++ src/app/components/homepage/shopping/orders/order/order.component.ts
@@ -1,60 +1,225 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewContainerRef} from '@angular/core';
+import {FormGroup, FormBuilder, Validators, FormControl} from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/map';
+import * as moment from 'moment';
+import { ActivatedRoute } from '@angular/router';
+import {ToastsManager} from "ng2-toastr";
+import {ExtracErrorMessages} from "../../../../../utils/ExtracMessages";
+import {OrderService} from '../../../../../services/shopping/order.service';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
-  styleUrls: ['./order.component.styl']
+  styleUrls: ['./order.component.styl'],
+  providers: [OrderService, ExtracErrorMessages]
 })
 export class OrderComponent implements OnInit {
 
-  arrayMaterial: Array<any>;
-  bodyTable: any;
+  product: FormControl;
+  unit: FormControl;
+  quantity: FormControl;
+  value: FormControl;
+  orderForm: FormGroup;
+  products: Array<any>;
+  productsSelected: Array<any>;
+  materials: Array<any>;
+  keys: Array<any>;
+  currentMaterial: any;
+  currentSupplier: any;
+  units: Array<any>;
+  productPlaceholder: string;
+  productClassStyle: string;
+  options: Array<any>;
+  suppliers: Array<any>;
+  readonly: Boolean;
 
-  constructor() {
-    this.arrayMaterial = [];
+  filteredOptions: Observable<any[]>;
+
+  constructor(
+    private _orderService: OrderService,
+    private _container: ViewContainerRef,
+    private fb: FormBuilder,
+    private _toast: ToastsManager,
+    private _extracErrorMessages: ExtracErrorMessages,
+    private _route: ActivatedRoute,
+  ){
+    this.products = [];
+    this.materials = [];
+    this.units = [];
+    this.keys = [];
+    this.options = [];
+    this.suppliers = [];
+    this.productsSelected = [];
+    this.productPlaceholder = '';
+    this.productClassStyle = '';
+    this.product = new FormControl();
+    this.unit = new FormControl();
+    this.quantity = new FormControl();
+    this.value = new FormControl();
+    this.currentMaterial = {};
+    this.readonly = false;
+    this._toast.setRootViewContainerRef(_container);
   }
 
   ngOnInit() {
-    this.bodyTable = document.getElementById('layout-body-order');
-    //this.addRow();
+    this.productPlaceholder = 'Producto';
+    this.productClassStyle = 'input-autocomplete';
+    this.keys = ['nombre', 'maquina', 'tiempo'];
+    this.unit.setValue("");
+    this.getUnits();
+    this.getDataProducts();
+    this.createForm();
+    this.orderForm.get('date').setValue(
+      moment().format('lll')
+    );
   }
 
-  addRow(){
-    this.bodyTable.insertAdjacentHTML('beforeend',this.rowTemplate())
+  createForm() {
+    this.orderForm = this.fb.group({ // <-- the parent FormGroup
+      date: ['', Validators.required ],
+      nit: ['', Validators.required ],
+      supplier: ['', Validators.required ],
+      address: ['', Validators.required ]
+    });
   }
 
-  rowTemplate(){
-    return "<div class='row-order'>" +
-      "<div class='t-body raw-material' _ngcontent-awl-10=''>" +
-        "<div class='input'>" +
-          "<input type='text' placeholder='Materia Prima'>" +
-        "</div>" +
-      "</div>" +
-      "<div class='t-body unit-measure' _ngcontent-awl-10=''>" +
-        "<div class='input'>" +
-          "<select name='unit' id=''>" +
-            "<option value=''>Seleccione una unidad</option>" +
-          "</select>" +
-        "</div>" +
-      "</div>" +
-      "<div class='t-body quantity' _ngcontent-awl-10=''>" +
-        "<div class='input'>" +
-         "<input type='text' placeholder='Cantidad'>" +
-        "</div>" +
-      "</div>" +
-      "<div class='t-body price' _ngcontent-awl-10=''>" +
-       "<div class='input'>" +
-          "<input type='text' placeholder='Valor Unit.'>" +
-        "</div>" +
-      "</div>" +
-      "<div class='t-body actions' _ngcontent-awl-10=''>" +
-        "<figure class='container-icons' (click)='addRow()'>" +
-          "<img src='assets/images/icons/add_icon.png' alt=''>" +
-       "</figure>" +
-        "<figure class='container-icons' (click)='deleteRow()'>" +
-          "<img src='assets/images/icons/delete_icon.png' alt=''>" +
-        "</figure>" +
-      "</div>" +
-    "</div>";
+  search(event: any) {
+    if (event.query.length > 2){
+      this._orderService.searchUnitMeasure(event.query)
+        .subscribe(
+          resp => this.options = resp.json(),
+          err => this._toast.error('Ocurrion un error obteniendo la materia prima', 'Pedidos')
+        );
+    }
+  }
+
+  selectData(data) {
+    this.currentMaterial = data;
+  }
+
+  searchSupplier(event: any) {
+    if (event.query.length > 2){
+      this._orderService.searchSupplier(event.query)
+        .subscribe(
+          resp => this.suppliers = resp.json(),
+          err => this._toast.error('Ocurrion un error obteniendo la materia prima', 'Pedidos')
+        );
+    }
+  }
+
+  selectDataSupplier(data) {
+    this.currentSupplier = data;
+    this.orderForm.get('nit').setValue(data.nit);
+    this.orderForm.get('address').setValue(data.direccion);
+    this.readonly = true;
+  }
+
+  getUnits(){
+    this._orderService.getDataUnits().subscribe(
+      (res) => {
+        let data = res.json();
+        this.units = data;
+      },
+      (err) => {
+        this.units = [];
+        console.log(err.json())
+      }
+    );
+  }
+
+  getDataProducts(){
+    this._orderService.getDataMaterial().subscribe(
+      (res) => {
+        let data = res.json();
+        this.materials = data;
+      },
+      (err) => {
+        this.materials = [];
+        console.log(err.json())
+      }
+    );
+  }
+
+  addRow() {
+    if (!this.currentMaterial.id || !this.unit.value || !this.quantity.value || !this.value.value) {
+      this._toast.info('Ingrese todos los datos del producto', 'Compras a Proveedores!');
+      return;
+    }
+
+    const data = {
+      materia_prima: this.currentMaterial.id,
+      materia_prima_id: this.currentMaterial.id,
+      unidad_medida: parseInt(this.unit.value),
+      unidad_medida_id: parseInt(this.unit.value),
+      cantidad: this.quantity.value,
+      valor_unitario: this.value.value
+    };
+
+    this.productsSelected.push(data);
+
+    const materia = this.currentMaterial;
+    const unit = this.units.find(data => data.id === parseInt(this.unit.value));
+    const quantity = this.quantity.value;
+    const value = this.value.value;
+
+    this.products.push({materia, unit, quantity, value});
+
+    this.currentMaterial = {};
+    this.product.setValue("");
+    this.unit.setValue("");
+    this.quantity.setValue("");
+    this.value.setValue("");
+
+  }
+
+  deleteRow(position) {
+    this.products = this.products.filter((product, index) => index !== position);
+    this.productsSelected = this.productsSelected.filter((product, index) => index !== position);
+  }
+
+  save() {
+    debugger
+    let newOrder = {};
+    if (this.orderForm.status === 'VALID') {
+      newOrder['fecha'] = moment().format('LLTS');
+      newOrder['proveedor'] = this.currentSupplier.id;
+      newOrder['proveedor_id'] = this.currentSupplier.id;
+      newOrder['total'] = 2000;
+      newOrder['usuario'] = 1;
+      newOrder['usuario_id'] = 1;
+      if (!this.productsSelected.length){
+        this._toast.info('Ingrese al menos un producto para la compra', 'Compra Materia Prima!');
+        return;
+      }
+
+      newOrder['materia_prima'] = this.productsSelected;
+
+      this._orderService.save(newOrder)
+        .subscribe(
+          (res) => {
+            let data = res.json();
+            this.orderForm.reset({
+              unit: ""
+            });
+            this.readonly = false;
+            this._toast.success(`Se guradó la compra`, 'Compra Materia Prima!');
+          },
+          (err) => {
+            if (err.status === 400){
+              let message = '';
+              message = this._extracErrorMessages.getMessages(err.json(), this.keys);
+              this._toast.info(message, 'Compra Materia Prima!', {toastLife: 10000})
+            }else{
+              this._toast.error('Ocurrio un error al crear', 'Compra Materia Prima!')
+            }
+            console.log(err.json())
+          }
+        );
+    }else{
+      this._toast.info('Todos los campos marcados con * son obligatorios', 'Campos obligatorios');
+    }
+
   }
 }
