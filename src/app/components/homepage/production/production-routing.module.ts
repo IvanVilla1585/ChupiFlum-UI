8e9a080f83ba77dbed0246c0f4a2238c354f9bbc
Ivan Villa <trezeguet55@gmail.com>
2017-12-02 07:41:43 -0500

    <production production-routing.module.ts>
    add new url

--- src/app/components/homepage/production/production-routing.module.ts
+++ src/app/components/homepage/production/production-routing.module.ts
@@ -35,6 +35,10 @@ export const routesProduction = {
       component: ProductComponent
     },
     {
+      path: 'productoterminado/:id',
+      component: ProductComponent
+    },
+    {
       path: 'tablaproductosterminados',
       component: TableproductsComponent
     },
