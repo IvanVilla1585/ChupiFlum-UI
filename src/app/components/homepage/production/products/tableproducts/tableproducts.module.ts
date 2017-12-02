7fdf5ea4d0c0db20b085ad9c08119ac2a3c6dcc9
Ivan Villa <trezeguet55@gmail.com>
2017-12-02 07:46:33 -0500

    <products tableproducts.module.ts>
    import MatSlideToggle

--- src/app/components/homepage/production/products/tableproducts/tableproducts.module.ts
+++ src/app/components/homepage/production/products/tableproducts/tableproducts.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableproductsRoutingModule } from './tableproducts-routing.module';
 import { TableproductsComponent } from './tableproducts.component';
+import {MatSlideToggle} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     TableproductsRoutingModule
   ],
-  declarations: [TableproductsComponent]
+  declarations: [TableproductsComponent, MatSlideToggle]
 })
 export class TableproductsModule { }
